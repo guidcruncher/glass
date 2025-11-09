ca8f9bacde2e4e95df7a941da9c8bc6c2ec0f084
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-11-09 05:42:09 +0000

    efoc

--- src/components/GlassAudioPlayer.vue
+++ src/components/GlassAudioPlayer.vue
@@ -224,111 +224,113 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="audio-player-container">
-    <!-- Hidden HTML Audio Element -->
-    <!-- The src attribute automatically updates when currentTrackData.src changes -->
-    <audio ref="audioElement" :src="currentTrackData.src" preload="metadata"></audio>
+  <div class="audio-player">
+    <div class="audio-player-container">
+      <!-- Hidden HTML Audio Element -->
+      <!-- The src attribute automatically updates when currentTrackData.src changes -->
+      <audio ref="audioElement" :src="currentTrackData.src" preload="metadata"></audio>
 
-    <div class="audio-player-wrapper">
-      <div class="track-info">
-        <div class="track-icon">
-          <!-- Music Note Icon SVG -->
-          <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <div class="audio-player-wrapper">
+        <div class="track-info">
+          <div class="track-icon">
+            <!-- Music Note Icon SVG -->
+            <svg viewBox="0 0 24 24" fill="var(--label)" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M12 3v10.55a4 4 0 1 1-2-3.46V7h4V5h-6V3h8M10 19a2 2 0 1 0 4 0 2 2 0 1 0-4 0z"
+              />
+            </svg>
+          </div>
+          <div class="track-details">
+            <span class="track-title">{{ currentTrackData.title || 'No Track Loaded' }}</span>
+            <span class="track-artist">{{ currentTrackData.artist || 'N/A' }}</span>
+          </div>
+        </div>
+
+        <!-- Glass Progress Bar (Custom) -->
+        <div class="glass-progress-bar-container" @click="handleSeekClick">
+          <div class="progress-bar-track" ref="progressBar">
+            <div
+              class="progress-bar-fill"
+              :style="{ width: `${progressPercentage}%` }"
+              :class="{ 'is-playing': isPlaying }"
+            >
+              <!-- Handle for dragging -->
+              <div class="progress-handle" @mousedown="startDrag" @touchstart="startDrag"></div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Time Display -->
+        <div class="time-display">
+          <span class="current-time">{{ formatTime(currentTime) }}</span>
+          <span class="duration">{{ formatTime(audioDuration) }}</span>
+        </div>
+
+        <!-- Controls -->
+        <div class="controls-row">
+          <button
+            class="glass-button control-button"
+            @click="handleTrackChange('previous')"
+            title="Previous Track"
+          >
+            <i class="fa-solid fa-chevron-left"></i>
+          </button>
+          <button
+            class="glass-button control-button"
+            @click="handleControl('rewind')"
+            title="Rewind 15s"
+          >
+            <i class="fa-solid fa-backward"></i>
+          </button>
+
+          <button
+            class="glass-button main-control-button"
+            @click="handleControl(isPlaying ? 'pause' : 'play')"
+            :title="isPlaying ? 'Pause' : 'Play'"
+          >
+            <i v-if="!isPlaying" class="fa-solid fa-play"></i>
+            <i v-else class="fa-solid fa-pause"></i>
+          </button>
+
+          <button class="glass-button control-button" @click="handleControl('stop')" title="Stop">
+            <i class="fa-solid fa-stop"></i>
+          </button>
+          <button
+            class="glass-button control-button"
+            @click="handleControl('fastforward')"
+            title="Fast Forward 15s"
+          >
+            <i class="fa-solid fa-forward"></i>
+          </button>
+          <button
+            class="glass-button control-button"
+            @click="handleTrackChange('next')"
+            title="Next Track"
+          >
+            <i class="fa-solid fa-chevron-right"></i>
+          </button>
+        </div>
+
+        <!--  Volume Slider -->
+        <div class="volume-control">
+          <svg viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 opacity-70">
+            <path d="M3 9v6h4l5 5V4L7 9z" />
+          </svg>
+          <input
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            v-model="volume"
+            class="glass-slider"
+            title="Volume Control"
+          />
+          <svg viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 opacity-70">
             <path
-              d="M12 3v10.55a4 4 0 1 1-2-3.46V7h4V5h-6V3h8M10 19a2 2 0 1 0 4 0 2 2 0 1 0-4 0z"
+              d="M14 3.23v2.06c2.89.81 5 3.53 5 6.71s-2.11 5.9-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77zM12 9L7 4v16l5-5z"
             />
           </svg>
         </div>
-        <div class="track-details">
-          <span class="track-title">{{ currentTrackData.title || 'No Track Loaded' }}</span>
-          <span class="track-artist">{{ currentTrackData.artist || 'N/A' }}</span>
-        </div>
-      </div>
-
-      <!-- Glass Progress Bar (Custom) -->
-      <div class="glass-progress-bar-container" @click="handleSeekClick">
-        <div class="progress-bar-track" ref="progressBar">
-          <div
-            class="progress-bar-fill"
-            :style="{ width: `${progressPercentage}%` }"
-            :class="{ 'is-playing': isPlaying }"
-          >
-            <!-- Handle for dragging -->
-            <div class="progress-handle" @mousedown="startDrag" @touchstart="startDrag"></div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Time Display -->
-      <div class="time-display">
-        <span class="current-time">{{ formatTime(currentTime) }}</span>
-        <span class="duration">{{ formatTime(audioDuration) }}</span>
-      </div>
-
-      <!-- Controls -->
-      <div class="controls-row">
-        <button
-          class="glass-button control-button"
-          @click="handleTrackChange('previous')"
-          title="Previous Track"
-        >
-          <i class="fa-solid fa-chevron-left"></i>
-        </button>
-        <button
-          class="glass-button control-button"
-          @click="handleControl('rewind')"
-          title="Rewind 15s"
-        >
-          <i class="fa-solid fa-backward"></i>
-        </button>
-
-        <button
-          class="glass-button main-control-button"
-          @click="handleControl(isPlaying ? 'pause' : 'play')"
-          :title="isPlaying ? 'Pause' : 'Play'"
-        >
-          <i v-if="!isPlaying" class="fa-solid fa-play"></i>
-          <i v-else class="fa-solid fa-pause"></i>
-        </button>
-
-        <button class="glass-button control-button" @click="handleControl('stop')" title="Stop">
-          <i class="fa-solid fa-stop"></i>
-        </button>
-        <button
-          class="glass-button control-button"
-          @click="handleControl('fastforward')"
-          title="Fast Forward 15s"
-        >
-          <i class="fa-solid fa-forward"></i>
-        </button>
-        <button
-          class="glass-button control-button"
-          @click="handleTrackChange('next')"
-          title="Next Track"
-        >
-          <i class="fa-solid fa-chevron-right"></i>
-        </button>
-      </div>
-
-      <!-- Volume Slider -->
-      <div class="volume-control">
-        <svg viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 opacity-70">
-          <path d="M3 9v6h4l5 5V4L7 9z" />
-        </svg>
-        <input
-          type="range"
-          min="0"
-          max="1"
-          step="0.01"
-          v-model="volume"
-          class="glass-slider"
-          title="Volume Control"
-        />
-        <svg viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 opacity-70">
-          <path
-            d="M14 3.23v2.06c2.89.81 5 3.53 5 6.71s-2.11 5.9-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77zM12 9L7 4v16l5-5z"
-          />
-        </svg>
       </div>
     </div>
   </div>
